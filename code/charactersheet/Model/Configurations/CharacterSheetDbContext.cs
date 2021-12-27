714cdd1ba4aabd41b64fcaca4dafd531b188142c
Sebastian Huber <shuber0802@gmail.com>
2021-12-27 12:48:18 +0100

    project setup

---
+++ code/charactersheet/Model/Configurations/CharacterSheetDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Model.Configurations;
+
+public class CharacterSheetDbContext : DbContext {
+    public CharacterSheetDbContext(DbContextOptions<CharacterSheetDbContext> options) : base(options) {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+    }
+}
