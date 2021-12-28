37baa19626167388ea3ad1242531ba6ab6e54be2
Sebastian Huber <shuber0802@gmail.com>
2021-12-28 22:43:06 +0100

    added SpellDetail page

---
+++ code/charactersheet/Domain/Repositories/Implementations/SpellRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Model.Configurations;
+using Model.Entities;
+
+namespace Domain.Repositories.Implementations;
+
+public class SpellRepository : ARepository<Spell>, ISpellRepository {
+    public SpellRepository(CharacterSheetDbContext context) : base(context) {
+    }
+
+    public async Task<Spell> ReadGraphAsync(string name) => (await _set
+        .Include(s => s.Classes)
+        .SingleOrDefaultAsync(s => s.Name == name))!;
+}
---
+++ code/charactersheet/Domain/Repositories/Interfaces/ISpellRepository.cs
@@ -0,0 +1,7 @@
+﻿using Model.Entities;
+
+namespace Domain.Repositories.Interfaces; 
+
+public interface ISpellRepository :IRepository<Spell> {
+    Task<Spell> ReadGraphAsync(string name);
+}
