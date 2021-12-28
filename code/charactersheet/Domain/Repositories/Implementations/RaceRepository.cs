91fc07499cb7e271d29de6391f1ee02ce152bbea
Sebastian Huber <shuber0802@gmail.com>
2021-12-28 10:41:41 +0100

    added Races (repo, page, ...)

---
+++ code/charactersheet/Domain/Repositories/Implementations/RaceRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Model.Configurations;
+using Model.Entities;
+
+namespace Domain.Repositories.Implementations;
+
+public class RaceRepository : ARepository<Race>, IRaceRepository {
+    public RaceRepository(CharacterSheetDbContext context) : base(context) {
+    }
+
+    public async Task<Race> ReadGraphAsync(int id) => await _set
+        .Include(s => s.ParentRace)
+        .Include(s => s.Traits)
+        .Include(s=>s.LanguageNames)
+        .Include(s=> s.InverseParentRace)
+        .SingleOrDefaultAsync(s => s.RaceId == id);
+    public async Task<Race> ReadGraphAsync(string name) => await _set
+        .Include(s => s.ParentRace)
+        .Include(s => s.Traits)
+        .Include(s=>s.LanguageNames)
+        .Include(s=> s.InverseParentRace)
+        .SingleOrDefaultAsync(s => s.Name == name);
+}
---
+++ code/charactersheet/Domain/Repositories/Interfaces/IRaceRepository.cs
@@ -0,0 +1,8 @@
+﻿using Model.Entities;
+
+namespace Domain.Repositories.Interfaces; 
+
+public interface IRaceRepository :IRepository<Race> {
+    Task<Race> ReadGraphAsync(int id);
+    Task<Race> ReadGraphAsync(string name);
+}
