6ed337e47d1cedb87b3588348100b542000c66b4
Alexander Dick <alexanderdick.punkt@gmail.com>
2021-12-28 12:08:31 +0100

    Classes + ClassDetail pages

---
+++ code/charactersheet/Domain/Repositories/Implementations/ClassRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Model.Configurations;
+using Model.Entities;
+
+namespace Domain.Repositories.Implementations;
+
+public class ClassRepository : ARepository<Class>, IClassRepository{
+    public ClassRepository(CharacterSheetDbContext context) : base(context){
+    }
+
+    public async Task<Class> ReadGraphAsync(int id) => (await _set
+        .Include(c => c.ClassHasSkillProficienciesChoices)
+        .Include(c => c.AbilityNames)
+        .Include(c => c.ArmorTypes)
+        .Include(c => c.Features)
+        .Include(c => c.Spells)
+        .Include(c => c.WeaponTypes)
+        .SingleOrDefaultAsync(c => c.ClassId == id))!;
+
+    public async Task<Class> ReadGraphAsync(string name) => (await _set
+        .Include(c => c.ClassHasSkillProficienciesChoices)
+        .Include(c => c.AbilityNames)
+        .Include(c => c.ArmorTypes)
+        .Include(c => c.Features)
+        .Include(c => c.Spells)
+        .Include(c => c.WeaponTypes)
+        .SingleOrDefaultAsync(c => c.Name == name))!;
+}
---
+++ code/charactersheet/Domain/Repositories/Interfaces/IClassRepository.cs
@@ -0,0 +1,8 @@
+﻿using Model.Entities;
+
+namespace Domain.Repositories.Interfaces; 
+
+public interface IClassRepository :IRepository<Class> {
+    Task<Class> ReadGraphAsync(int id);
+    Task<Class> ReadGraphAsync(string name);
+}
