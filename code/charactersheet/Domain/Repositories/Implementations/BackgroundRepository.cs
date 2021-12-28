2864aa8c44fe5e2caccb0abcdc620ea845efcb4c
Alexander Dick <alexanderdick.punkt@gmail.com>
2021-12-28 12:08:56 +0100

    Backgrounds page

---
+++ code/charactersheet/Domain/Repositories/Implementations/BackgroundRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Repositories.Interfaces;
+using Model.Configurations;
+using Model.Entities;
+
+namespace Domain.Repositories.Implementations;
+
+public class BackgroundRepository:ARepository<Background>{
+    public BackgroundRepository(CharacterSheetDbContext context) : base(context){
+    }
+}
