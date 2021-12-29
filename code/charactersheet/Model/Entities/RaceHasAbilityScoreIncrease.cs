7722fd44401b4091990afc8c90f0acafdf63d2d0
KonstiTheProgrammer <Konstantin.Kanzlerr@gmail.com>
2021-12-29 11:27:04 +0100

    added class race has abiltiy score increase

---
+++ code/charactersheet/Model/Entities/RaceHasAbilityScoreIncrease.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.Entities
+{
+    public partial class RaceHasAbilityScoreIncrease
+    {
+        public int RaceId { get; set; }
+        public string AbilityName { get; set; } = null!;
+        public int Increase { get; set; }
+
+        public virtual EAbilityName AbilityNameNavigation { get; set; } = null!;
+        public virtual Race Race { get; set; } = null!;
+    }
+}
