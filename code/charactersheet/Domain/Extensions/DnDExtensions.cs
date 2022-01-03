ce6ce7276ac41a9ce7cc27f2573a0905d6bfebb7
Sebastian Huber <shuber0802@gmail.com>
2022-01-03 22:44:40 +0100

    updated CharacterSheet (styling, displaying initial properties)

--- code/charactersheet/Domain/Extensions/DnDExtensions.cs
+++ code/charactersheet/Domain/Extensions/DnDExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Domain.Extensions;
+﻿using Model.Entities;
+
+namespace Domain.Extensions;
 
 public static class DnDExtensions {
     private static Random _random = new Random();
@@ -9,11 +11,40 @@ public static class DnDExtensions {
             rolls.Add(_random.Next(1, type + 1));
         }
 
-        if (dropLowest) 
+        if (dropLowest)
             rolls.Remove(rolls.Min());
-        
+
         return rolls;
     }
 
-    public static string[] AbilitiesSorted { get; } = { "STRENGTH", "DEXTERITY", "CONSTITUTION", "INTELLIGENCE", "WISDOM", "CHARISMA" };
+    public static string[] AbilitiesSorted { get; } =
+        { "STRENGTH", "DEXTERITY", "CONSTITUTION", "INTELLIGENCE", "WISDOM", "CHARISMA" };
+
+    public static int GetAbilityModifier(this int abilityScore) =>
+        Convert.ToInt32(Math.Floor(Convert.ToDecimal((abilityScore - 10m) / 2m)));
+    
+    public static int GetAbilityModifier(this Ability ability) =>
+        Convert.ToInt32(Math.Floor(Convert.ToDecimal((ability.AbilityScore - 10m) / 2m)));
+    
+    public static int ModifierWithPossibleProficiencyBonus(this Ability ability, int proficiencyBonus) {
+        var abilityModifier = ability.AbilityScore.GetAbilityModifier();
+        if (ability.Proficient == 1) 
+            return abilityModifier + 2; // + proficiency bonus
+        
+        return abilityModifier;
+    }
+    public static int ModifierWithPossibleProficiencyBonus(this Skill skill, int proficiencyBonus) {
+        var abilityModifier = skill.Ability.AbilityScore.GetAbilityModifier();
+        if (skill.Proficient == 1) 
+            return abilityModifier + 2; // + proficiency bonus
+        
+        return abilityModifier;
+    }
+    
+    public static string AddSign(this int number) {
+        if (Math.Sign(number) == -1) 
+            return number.ToString();
+        
+        return "+" + number;
+    }
 }
