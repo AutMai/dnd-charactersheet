ae57f77bc294df1b2ebfd7c68ada421c109d9631
Sebastian Huber <shuber0802@gmail.com>
2022-01-06 20:11:02 +0100

    updated styling in info pages

--- code/charactersheet/Domain/Repositories/Interfaces/IClassRepository.cs
+++ code/charactersheet/Domain/Repositories/Interfaces/IClassRepository.cs
@@ -5,4 +5,5 @@ namespace Domain.Repositories.Interfaces;
 public interface IClassRepository :IRepository<Class> {
     Task<Class> ReadGraphAsync(int id);
     Task<Class> ReadGraphAsync(string name);
+    Task<List<Class>> ReadGraphAsync();
 }
--- code/charactersheet/Domain/Repositories/Interfaces/IRaceRepository.cs
+++ code/charactersheet/Domain/Repositories/Interfaces/IRaceRepository.cs
@@ -1,9 +1,11 @@
-﻿using Model.Entities;
+﻿using Domain.Repositories.Implementations;
+using Model.Entities;
 
 namespace Domain.Repositories.Interfaces; 
 
 public interface IRaceRepository :IRepository<Race> {
     Task<Race> ReadGraphAsync(int id);
     Task<Race> ReadGraphAsync(string name);
+    Task<List<Race>> ReadGraphAsync();
     Task<List<Race>> ReadParentRaces();
 }
