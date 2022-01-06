a7ed9f4e9f0fe2b3043dae51b46d8cd7245f9c81
Sebastian Huber <shuber0802@gmail.com>
2022-01-06 11:58:09 +0100

    updated migrationScript

--- code/charactersheet/migrationScript.sh
+++ code/charactersheet/migrationScript.sh
@@ -1,4 +1,7 @@
+export MYSQL_PWD=root;"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root -e "DROP DATABASE dnd;CREATE DATABASE dnd;"
+
 cd Model
+rm -rf Migrations
 
 echo 'Enter migration name:'
 read migrationName
@@ -6,3 +9,5 @@ read migrationName
 dotnet ef -s ../View migrations add "$migrationName"
 
 dotnet ef -s ../View database update
+
+export MYSQL_PWD=root;"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u root dnd < dnd_inserts.sql
